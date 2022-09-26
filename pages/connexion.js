b8be9200b66c7ecfbcc35e4fb6f0a106dd406231
LeonardRingot <leonardringot6@gmail.com>
2022-09-26 18:48:08 +0200

    commitpcdeux

--- pages/connexion.js
+++ pages/connexion.js
@@ -1,18 +1,20 @@
 export default function connexion() 
 {
 return (
+  <div>
+    <h1>iuezkje</h1>
     <form action="" method="post">
-  <label for="pseudo">pseudo:</label>
-  <input type="text"  name="pseudo" />
+      <label for="pseudo">pseudo:</label>
+      <input type="text"  name="pseudo" />
 
-  <label for="email">mail:</label>
-  <input type="email"  name="email" />
+      <label for="email">mail:</label>
+      <input type="email"  name="email" />
 
-  <label for="password">Mot de passe:</label>
-  <input type="password"  name="password" />
+      <label for="password">Mot de passe:</label>
+      <input type="password"  name="password" />
 
-  <button type="submit">Submit</button>
-</form>
-    
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 );
 }
